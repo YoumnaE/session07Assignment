aec6a38b819027e8a54010b50545365ef1cae299
YoumnaE <yomnaelsayed3@gmail.com>
2026-09-20 11:32:41 +0300

    Q1

--- session07Assignment/Program.cs
+++ session07Assignment/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            Book book = new Book();
+            //Console.WriteLine(Book.password);
+            //password is only accessible within its own class
+            #endregion
         }
     }
 }
